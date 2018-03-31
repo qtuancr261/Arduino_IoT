777f7abc8e42337ddc8330b0289e1d36d1585936
qtuancr261 <tuancr261@gmail.com>
2018-03-31 16:16:23 +0700

    add alertLEDs() function  to indicate to user the status of bitcoin rate

--- sketch_feb09a_ESP8266ArduinoShield/sketch_feb09a_ESP8266ArduinoShield.ino
+++ sketch_feb09a_ESP8266ArduinoShield/sketch_feb09a_ESP8266ArduinoShield.ino
@@ -2,19 +2,26 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
-#define LED D0
+
+#define LED_YELLOW D10
+#define LED_GREEN D8
 const char* ssid     = "RedmiTQT";
+const char* ssid_alt_1 = "Trasua_tokyo";
 const char* password = "261261261";
+const char* password_alt_1 = "abcd1234";
 const char* host = "tungtu-btc.herokuapp.com";
 const int httpPort = 80;
+const int led_red = 16;
+const int led_green = 14;
 ESP8266WebServer server(80); // HTTP server on port 80
 //---------------------------------------------------------------
-String getResponse( String url) {
+String getResponse( String url) 
+{
 
    WiFiClient client;
 
   if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
+    //Serial.println("connection failed");
     return "failed";
   }
     //Send our request
@@ -38,10 +45,14 @@ String getResponse( String url) {
   
   return reply;
 }
+//---------------------------------------------------------------
 void getBitcoinInformation(String APIUrl)
 {
   String replyFromServer = getResponse(APIUrl);
   //Serial.print(replyFromServer);
+  // No more shit from ESP8266
+  if (replyFromServer == "failed")
+    return;
   String data;
   int jsonIndex = 0;
   for (int i = 0; i < replyFromServer.length(); i++)
@@ -58,24 +69,59 @@ void getBitcoinInformation(String APIUrl)
   JsonObject& root = jsonBuffer.parseObject(data);
   // Notify arduino
   Serial.print("start");
-  delay(1000);
+  digitalWrite(led_red, HIGH);
+  
+  delay(500);
   // Get time updated
   String time = root["results"]["time"]["time_update"];
   Serial.print(time);
-  delay(1000);
+  digitalWrite(led_red, LOW);
+  delay(500);
   // Get USD rate
   String usdRate = root["results"]["bpi"]["USD"]["rate"];
   double usd = root["results"]["bpi"]["USD"]["rate_float"];
   Serial.print(usdRate +"$");
+  digitalWrite(led_red, HIGH);
   //Serial.println(usd);
-  delay(1000);
+  delay(500);
   // Get VN rate
   String vnRate = root["results"]["bpi"]["VND"]["rate"];
   double vnd = root["results"]["bpi"]["VND"]["rate_float"];
   Serial.print(vnRate);
+  digitalWrite(led_red, LOW);
   //Serial.println(vnd);
-  delay(1000);
+  delay(500);
+  alertLEDs(usd);
 }
+ //-----------------------------------------------------------------------------
+ void alertLEDs(double currentRate)
+ {
+    static double preRate = 0;
+    static double threshold = 0.5; // usd
+    if (preRate == 0)
+        preRate = currentRate;
+    else 
+    {
+        if (currentRate > (preRate + threshold))
+        {
+          digitalWrite(led_green, HIGH);
+          delay(50);
+          digitalWrite(led_green, LOW);
+          delay(50);
+        }
+        else
+        {
+          digitalWrite(led_green, HIGH);
+          delay(50);
+          digitalWrite(led_green, LOW);
+          delay(50);
+          digitalWrite(led_green, HIGH);
+          delay(50);
+          digitalWrite(led_green, LOW);
+          delay(50);
+        }
+    }
+ }
 //------------------------------------------------------------------------------
 void setup() {
  Serial.begin(115200);    
@@ -83,7 +129,11 @@ void setup() {
  // Disconnect AP
  WiFi.mode(WIFI_STA);  
  WiFi.begin(ssid, password); // Connect to WIFI network
- pinMode(LED, OUTPUT);
+ // Setup LEDs
+ pinMode(led_red, OUTPUT);
+ pinMode(led_green, OUTPUT);
+ //pinMode(LED_GREEN, OUTPUT);
+ //pinMode(LED_YELLOW, OUTPUT);
 // Wait for connection
  while (WiFi.status() != WL_CONNECTED) {
   delay(500);
